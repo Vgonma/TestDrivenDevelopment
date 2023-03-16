e33e1fa04a4d798c35f62582b9d69ed8b706922c
Victor <victorgmassimi.microverse@gmail.com>
2023-03-16 14:01:59 -0600

    add Solver#fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../lib/solver'
 
-describe Solver do 
+describe Solver do
   context 'testing methods' do
-    before :each do 
+    before :each do
       @solver = Solver.new
     end
 
-    it '#factorial returns correct factorial' do 
+    it '#factorial returns correct factorial' do
       expect(@solver.factorial(0)).to eql(1)
       expect(@solver.factorial(3)).to eql(6)
-      expect(@solver.factorial(10)).to eql(3628800)
+      expect(@solver.factorial(10)).to eql(3_628_800)
     end
 
-    it '#reverse return reversed string' do 
+    it '#reverse return reversed string' do
       expect(@solver.reverse('hello')).to eql('olleh')
       expect(@solver.reverse('hello world')).to eql('dlrow olleh')
     end
