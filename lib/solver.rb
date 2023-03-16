135690382f8bf81e397309d1ccc472d055d29130
AlejandraKeber <alejandrakeber@gmail.com>
2023-03-16 14:54:58 -0500

    Add methods for factorial and reverse

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,2 +1,13 @@
 class Solver
+  def factorial(number)
+    if number.zero?
+      1
+    else
+      number * factorial(number - 1)
+    end
+  end
+
+  def reverse(str)
+    str.reverse
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,20 +7,21 @@ describe Solver do
     end
 
     it '#factorial returns correct factorial' do 
-      expect(solver.factorial(0)).to eql(1)
-      expect(solver.factorial(3)).to eql(6)
-      expect(solver.factorial(10)).to eql(3628800)
+      expect(@solver.factorial(0)).to eql(1)
+      expect(@solver.factorial(3)).to eql(6)
+      expect(@solver.factorial(10)).to eql(3628800)
     end
 
     it '#reverse return reversed string' do 
-      expect(solver.reverse('hello')).to eql('olleh')
-      expect(solver.reverse('hello world')).to eql('olleh dlrow')
+      expect(@solver.reverse('hello')).to eql('olleh')
+      expect(@solver.reverse('hello world')).to eql('dlrow olleh')
     end
 
     it '#fizzbuzz return correct value' do
-      expect(solver.fizzbuzz(3)).to eql('fizz')
-      expect(solver.fizzbuzz(5)).to eql('buzz')
-      expect(solver.fizzbuzz(15)).to eql('fizzbuzz')
-      expect(solver.fizzbuzz(4)).to eql('4')
+      expect(@solver.fizzbuzz(3)).to eql('fizz')
+      expect(@solver.fizzbuzz(5)).to eql('buzz')
+      expect(@solver.fizzbuzz(15)).to eql('fizzbuzz')
+      expect(@solver.fizzbuzz(4)).to eql('4')
+    end
   end
 end
